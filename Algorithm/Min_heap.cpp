830e5b9ed3c8147339afe9e1aaf425b8d69b8f14
Badar <badar.personal.ds@gmail.com>
2025-04-28 13:14:16 +0600

    initial commit

--- Algorithm/Min_heap.cpp
+++ Algorithm/Min_heap.cpp
@@ -38,8 +38,8 @@ public:
 
     // Function to heapify downwards 
     void HeapDown(int i) {
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+        int left = 2 * i + 1; // left child
+        int right = 2 * i + 2; // right child
         int smallest = i;
 
         // If left child is smaller than current node
